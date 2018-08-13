93b364210e7d7518481c8a1d2a84945bcd529fe1
Damien Abeloos <dabeloos@w-r2-p2.s19.be>
2018-08-13 11:59:01 +0200

    ex04 ft_fibonacci

---
+++ ex04/ft_fibonacci.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/13 10:19:41 by dabeloos          #+#    #+#             */
+/*   Updated: 2018/08/13 10:41:19 by dabeloos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_fibonacci(int index)
+{
+	if (index == 0)
+		return (0);
+	else if (index == 1)
+		return (1);
+	else if (index > 1)
+		return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+	else
+		return (-1);
+}
