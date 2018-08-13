ebae4130393c935882c38e7046b1c89f1ac46a8e
Damien Abeloos <dabeloos@w-r2-p2.s19.be>
2018-08-13 10:18:32 +0200

    ex03 ft_recursive_power

--- ex03/ft_recursive_power.c
+++ ex03/ft_recursive_power.c
@@ -6,14 +6,16 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/12 18:43:11 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/12 18:47:54 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/13 10:17:29 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_recursive_power(int nb, int power)
 {
-	if (power > 1)
-		return nb * ft_recursive_power(nb, power - 1);
+	if (power > 0)
+		return (nb * ft_recursive_power(nb, power - 1));
 	else if (power == 0)
-		return 
+		return (1);
+	else
+		return (0);
 }
