ec335d51c3293b64ff709a005b1e5c94717a4c62
Damien Abeloos <dabeloos@w-r2-p2.s19.be>
2018-08-14 15:30:09 +0200

    finished

--- ex08/ft_eight_queens_puzzle.c
+++ ex08/ft_eight_queens_puzzle.c
@@ -6,34 +6,14 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/13 14:49:37 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/14 14:49:07 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/14 15:13:24 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdlib.h>
-#include <stdio.h>
 
 struct				s_chessboard
 {
 	int		chessboard[8][8];
 };
-
-void				ft_show_board(struct s_chessboard board)
-{
-	int	l;
-	int	c;
-
-	l = -1;
-	while (++l < 8)
-	{
-		c = -1;
-		while (++c < 8)
-		{
-			printf(" %d ", board.chessboard[l][c]);
-		}
-		printf("\n");
-	}
-}
 
 struct s_chessboard	ft_initialize_array(void)
 {
@@ -130,20 +110,4 @@ int					ft_eight_queens_puzzle(void)
 
 	board = ft_initialize_array();
 	return (ft_put_a_queen(board, 0));
-}
-
-int					main(int argc, char *argv[])
-{
-	struct s_chessboard	board;
-	int					c;
-
-	board = ft_initialize_array();
-	if (argc == 3)
-	{
-		board = ft_restrict_lines(board, atoi(argv[1]), atoi(argv[2]));
-		ft_show_board(board);
-	}
-	c = ft_eight_queens_puzzle();
-	printf("%d", c);
-	return (0);
 }
