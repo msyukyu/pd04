aac8a1d485d1dc66db275883bfa4a88ea60d5194
Damien Abeloos <dabeloos@w-r2-p2.s19.be>
2018-08-14 14:33:46 +0200

    finished

--- ex08/ft_eight_queens_puzzle.c
+++ ex08/ft_eight_queens_puzzle.c
@@ -6,14 +6,19 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/13 14:49:37 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/14 12:41:50 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/14 14:33:35 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
 
-void    ft_show_board(int board[][8])
+struct		chessboard
+{
+	int		chessboard[8][8];
+};
+
+void	ft_show_board(struct chessboard board)
 {
 	int     l;
 	int     c;
@@ -24,16 +29,17 @@ void    ft_show_board(int board[][8])
 		c = -1;
 		while (++c < 8)
 		{
-			printf(" %d ", board[l][c]);
+			printf(" %d ", board.chessboard[l][c]);
 		}
 		printf("\n");
 	}
 }
 
-void	ft_initialize_array(int board[][8])
+struct chessboard	ft_initialize_array(void)
 {
 	int		l;
 	int		c;
+	struct chessboard	board;
 
 	l = -1;
 	while (++l < 8)
@@ -41,31 +47,32 @@ void	ft_initialize_array(int board[][8])
 		c = -1;
 		while (++c < 8)
 		{
-			board[l][c] = 0;
+			board.chessboard[l][c] = 0;
 		}
 	}
+	return (board);
 }
 
-int		(*ft_copy_board(int board[][8]))[8]
+struct chessboard	ft_copy_board(struct chessboard board)
 {
-	int static	newboard[8][8];
+	struct chessboard	newboard;
 	int			c;
 	int			l;
 
-	ft_initialize_array(newboard);
+	newboard = ft_initialize_array();
 	l = -1;
 	while (++l < 8)
 	{
 		c = -1;
 		while (++c < 8)
 		{
-			newboard[l][c] = board[l][c];
+			newboard.chessboard[l][c] = board.chessboard[l][c];
 		}
 	}
 	return (newboard);
 }
 
-void	ft_restrict_lines(int board[][8], int cl, int cc)
+struct chessboard	ft_restrict_lines(struct chessboard board, int cl, int cc)
 {
 	int		l;
 	int		c;
@@ -74,41 +81,43 @@ void	ft_restrict_lines(int board[][8], int cl, int cc)
 	c = -1;
 	while (++c < 8)
 	{
-		if (board[cl][c] == 0)
-			board[cl][c] = cl + 1;
-		if (board[c][cc] == 0)
-			board[c][cc] = cl + 1;
+		if (board.chessboard[cl][c] == 0)
+			board.chessboard[cl][c] = cl + 1;
+		if (board.chessboard[c][cc] == 0)
+			board.chessboard[c][cc] = cl + 1;
 	}
 	c = -8;
 	while (++c < 8)
 	{
 		if (cc - c > -1 && cl - c > -1 && cc - c < 8 && cl - c < 8 &&
-				board[cl - c][cc - c] == 0)
-			board[cl - c][cc - c] = cl + 1;
+				board.chessboard[cl - c][cc - c] == 0)
+			board.chessboard[cl - c][cc - c] = cl + 1;
 		if (cc + c > -1 && cl - c > -1 && cc + c < 8 && cl - c < 8 &&
-				board[cl - c][cc + c] == 0)
-			board[cl - c][cc + c] = cl + 1;
+				board.chessboard[cl - c][cc + c] == 0)
+			board.chessboard[cl - c][cc + c] = cl + 1;
 	}
+	return (board);
 }
 
-int		ft_put_a_queen(int board[][8], int line)
+int		ft_put_a_queen(struct chessboard board, int line)
 {
 	int		c;
 	int		count;
+	struct chessboard	newboard;
 
 	count = 0;
 	c = 0;
 	while (c < 8)
 	{
-		if (board[line][c] == 0)
+		if (board.chessboard[line][c] == 0)
 		{
-			ft_restrict_lines(board, line, c);
+			newboard = ft_restrict_lines(board, line, c);
 			if (line == 7)
 			{
 				count++;
 			}
 			else
-				count += ft_put_a_queen(ft_copy_board(board), line + 1);
+				count += ft_put_a_queen(ft_copy_board(newboard), line + 1);
 		}
 		c++;
 	}
@@ -117,30 +126,21 @@ int		ft_put_a_queen(int board[][8], int line)
 
 int		ft_eight_queens_puzzle(void)
 {
-	int		board[8][8];
+	struct chessboard	board;
 
-	ft_initialize_array(board);
+	board = ft_initialize_array();
 	return (ft_put_a_queen(board, 0));
 }
 
 int		main(int argc, char *argv[])
 {
-	int		board[8][8];
-	int		l;
+	struct chessboard	board;
 	int		c;
 
-	l = -1;
-	while (++l < 8)
-	{
-		c = -1;
-		while (++c < 8)
-		{
-			board[l][c] = 0;
-		}
-	}
+	board = ft_initialize_array();
 	if (argc == 3)
 	{
-		ft_restrict_lines(board, atoi(argv[1]), atoi(argv[2]));
+		board = ft_restrict_lines(board, atoi(argv[1]), atoi(argv[2]));
 		ft_show_board(board);
 	}
 	c = ft_eight_queens_puzzle();
